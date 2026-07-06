1e1cb17a8010ef646d87063541165a4d07a7b11b
Deebakkarthi Chinnasame Rani <dbk@deebakkarthi.com>
2026-07-06 15:58:49 -0400

    Add ethmac

---
+++ bench/comm/ethmac/ethmac.f
@@ -0,0 +1,31 @@
+# vim: set syntax=verilog :
+/*Command file derived from ethmac.core file*/
+./rtl/ethmac.v
+
+./rtl/eth_clockgen.v
+./rtl/eth_cop.v
+./rtl/eth_crc.v
+./rtl/eth_fifo.v
+./rtl/eth_maccontrol.v
+./rtl/eth_macstatus.v
+./rtl/eth_miim.v
+./rtl/eth_outputcontrol.v
+./rtl/eth_random.v
+./rtl/eth_receivecontrol.v
+./rtl/eth_registers.v
+./rtl/eth_rxaddrcheck.v
+./rtl/eth_rxcounters.v
+./rtl/eth_register.v
+./rtl/eth_rxethmac.v
+./rtl/eth_rxstatem.v
+./rtl/eth_spram_256x32.v
+./rtl/eth_shiftreg.v
+./rtl/eth_transmitcontrol.v
+./rtl/eth_txcounters.v
+./rtl/eth_txethmac.v
+./rtl/eth_txstatem.v
+./rtl/eth_wishbone.v
+
++incdir+./rtl
+./rtl/ethmac_defines.v
+./rtl/timescale.v
