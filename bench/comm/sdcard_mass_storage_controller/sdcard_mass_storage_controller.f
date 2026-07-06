af308e984eb9efa53c80369b431b648f75f10cff
Deebakkarthi Chinnasame Rani <dbk@deebakkarthi.com>
2026-07-06 16:28:42 -0400

    Add sdcard_mass_storage_controller

---
+++ bench/comm/sdcard_mass_storage_controller/sdcard_mass_storage_controller.f
@@ -0,0 +1,16 @@
+/*AUTO-GENERATED USING gen_command_file.sh*/
++incdir+./rtl
+./rtl/sd_bd.v
+./rtl/sd_clock_divider.v
+./rtl/sd_cmd_master.v
+./rtl/sd_cmd_serial_host.v
+./rtl/sd_controller_wb.v
+./rtl/sd_crc_7.v
+./rtl/sd_crc_16.v
+./rtl/sd_data_master.v
+./rtl/sd_data_serial_host.v
+./rtl/sd_fifo_rx_filler.v
+./rtl/sd_fifo_tx_filler.v
+./rtl/sd_rx_fifo.v
+./rtl/sd_tx_fifo.v
+./rtl/sdc_controller.v
