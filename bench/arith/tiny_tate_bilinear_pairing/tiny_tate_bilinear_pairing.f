9548ef09e1c958ad2b078738c08088d86571619a
Deebakkarthi Chinnasame Rani <dbk@deebakkarthi.com>
2026-07-06 15:32:35 -0400

    add tiny_tate_bilinear_pairing

---
+++ bench/arith/tiny_tate_bilinear_pairing/tiny_tate_bilinear_pairing.f
@@ -0,0 +1,10 @@
+# vim: syntax=verilog :
+/*AUTO-GENERATED USING gen_command_file.sh*/
+./rtl/const.v
+./rtl/cubic.v
+./rtl/fsm.v
+./rtl/pairing.v
+./rtl/pe.v
+./rtl/ram.v
+./rtl/rom.v
+./rtl/tiny.v
