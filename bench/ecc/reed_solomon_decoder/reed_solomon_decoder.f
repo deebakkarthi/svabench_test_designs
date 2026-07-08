ddd495fe6550edc0a7d1f1b1d045233a02c614d2
Deebakkarthi Chinnasame Rani <dbk@deebakkarthi.com>
2026-07-08 18:47:12 -0400

    Add reed solomon decoder

---
+++ bench/ecc/reed_solomon_decoder/reed_solomon_decoder.f
@@ -0,0 +1,13 @@
+/*AUTO-GENERATED USING gen_command_file.sh*/
+./rtl/BM_lamda.v
+./rtl/DP_RAM.v
+./rtl/GF_matrix_ascending_binary.v
+./rtl/GF_matrix_dec.v
+./rtl/GF_mult_add_syndromes.v
+./rtl/Omega_Phy.v
+./rtl/RS_dec.v
+./rtl/error_correction.v
+./rtl/input_syndromes.v
+./rtl/lamda_roots.v
+./rtl/out_stage.v
+./rtl/transport_in2out.v
