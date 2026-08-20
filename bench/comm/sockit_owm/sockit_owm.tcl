9f82bd6d3be87793e85def6c3dceb97bb881d3a0
Deebakkarthi Chinnasame Rani <dbk@deebakkarthi.com>
2026-08-20 15:38:39 -0400

    Add tcl for sockit_owm

---
+++ bench/comm/sockit_owm/sockit_owm.tcl
@@ -0,0 +1,15 @@
+clear -all
+
+analyze -sv -f sockit_owm.f
+
+if { [file exists sockit_owm_sva.f] == 1 } {
+	analyze -sv -f sockit_owm_sva.f
+}
+
+elaborate
+clock clk
+reset -none
+
+prove -all
+llength [get_property_list -include {type {assert} status {proven} related_cover_status {green white}}]
+exit
