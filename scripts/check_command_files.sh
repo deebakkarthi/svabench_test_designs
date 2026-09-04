4f9ba5bcc90edf2d332fadbd9932153696303cf7
Deebakkarthi Chinnasame Rani <dbk@deebakkarthi.com>
2026-09-04 09:41:36 -0400

    fix: Use array instead of bash expansion

--- scripts/check_command_files.sh
+++ scripts/check_command_files.sh
@@ -8,6 +8,13 @@ usage() {
 	echo -e " If no DIR is provided, run on CWD"
 }
 
+# Check if we have iverilog
+if ! command -v iverilog >/dev/null 2>&1
+then
+	echo -ne "$progname: iverilog cannot be found\n"
+	exit 1
+fi
+
 input_dir="."
 if [[ "$#" -gt 0 ]]; then
 	if [[ "$1" == "-h" ]]; then
@@ -17,7 +24,6 @@ if [[ "$#" -gt 0 ]]; then
 		input_dir="$1"
 	fi
 fi
-input_dir=$(realpath "$input_dir")
 
 if [[ ! -d "$input_dir" ]]; then
 	echo -e "$progname: $input_dir doesn't exist"
@@ -28,20 +34,25 @@ declare -i passed
 declare -i failed
 declare -i total
 declare -i curr
-total=$(find "$input_dir" -depth -mindepth 1 -maxdepth 1  -type f -name "*f" | wc -l)
+
+mapfile -t command_files_arr < <(find "$input_dir" -depth -mindepth 1 -maxdepth 1  -type f -name "*f")
+
+total="${#command_files_arr[@]}"
 curr=1
+passed=0
+failed=0
 # Change dir because .f files have relative paths
 pushd $input_dir > /dev/null
-for file in "$input_dir"/*.f; do
-	echo -en "[$curr/$total] Testing $file\033[K\r"
-	if iverilog -f "$file" >/dev/null 2>&1; then
+for file in "${command_files_arr[@]}"; do
+	echo -en "[$curr/$total] Testing $file\n"
+	if iverilog -f "$(basename $file)" >/dev/null 2>&1; then
 		((passed++))
 	else
 		((failed++))
 	fi
 	((curr++))
 done
-echo -e "\033[K\r[$passed/$total] compiled successfully"
+echo -ne "[$passed/$total] compiled successfully\n"
 rm -rf "./a.out"
 # Return back to the original dir
 popd > /dev/null
